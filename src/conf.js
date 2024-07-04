507693b32c1c5f8eb5bb05938a922a079d936b35
Binnette <binnette@gmail.com>
2024-07-04 04:28:02 +0200

    Add public client_id for local debug

--- src/conf.js
+++ src/conf.js
@@ -1,4 +1,4 @@
-const client_id = '...';
+const client_id = '8fPnRt71H3eVzk_MDjxUbqY9YLobiBSxwzzdbZ5T_58';
 const url = 'https://www.openstreetmap.org';
 const apiUrl = 'https://api.openstreetmap.org';
 export const overpassApiUrl = 'https://overpass-api.de/api/';
