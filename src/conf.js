377b3a746344bf8eb0a88f245c622351a76f1ca9
Binnette <binnette@gmail.com>
2024-07-05 13:38:02 +0200

    Dynamic configuration

--- src/conf.js
+++ src/conf.js
@@ -1,4 +1,15 @@
-const client_id = '8fPnRt71H3eVzk_MDjxUbqY9YLobiBSxwzzdbZ5T_58';
+let client_id = "";
+if (window.location.toString().startsWith('https://binnette.github.io/parking-mapper/')) {
+  // PROD configuration
+  client_id = 'vchtknFnqUHquSUleQSXeLmi2Jt2LEi6CT-6lJOVVsY';
+  console.log("Prod configuration");
+} else if (window.location.toString().startsWith('https://localhost:5173/parking-mapper'))  {
+  // DEV configuration
+  client_id = '8fPnRt71H3eVzk_MDjxUbqY9YLobiBSxwzzdbZ5T_58';
+  console.log("Dev configuration");
+} else {
+  console.error('Check configuration. Unknow hostname: ' + window.location.hostname);
+}
 const url = 'https://www.openstreetmap.org';
 const apiUrl = 'https://api.openstreetmap.org';
 export const overpassApiUrl = 'https://overpass-api.de/api/';
